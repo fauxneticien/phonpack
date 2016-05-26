eb048e6921d0612bfcb218b07bb7d76c2ff2831b
Nay San <nay.san@mq.edu.au>
2016-05-26 17:08:31 +1000

    Added initial alpha version of writeTextGrid function

---
+++ R/writeTextGrid.R
@@ -0,0 +1,24 @@
+#' Write data to a Praat TextGrid file
+#'
+#' Write data to a TextGrid file into R
+#' @param tg TextGrid data, returned from readTextGrid() or createTextGrid() functions
+#' @param file a character string of the filename to write to
+#' @export
+#' @examples
+#' Write to the current working directory
+#' writeTextGrid(tg_data, "my_tg.TextGrid")
+#'
+#' Write to specific file path
+#' readTextGrid(tg_data, "path/to/textgrids/thisfile.TextGrid")
+
+writeTextGrid <- function(tg, file) {
+
+  ctx <- v8()
+  ctx$source(system.file("js/JSONtoTextGrid.min.js",
+                         package = "phonpack"))
+
+  tg_str <- ctx$call("JSONtoTextGrid", tg)
+
+  writeLines(tg_str, file)
+
+}
