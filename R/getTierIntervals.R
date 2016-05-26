1b4a887564e2105599a0bcb2a26483d6c70fd12a
Nay San <nay.san@mq.edu.au>
2016-05-26 23:39:25 +1000

    getTierIntervals() defaults to FALSE for discarding empty rows now

--- R/getTierIntervals.R
+++ R/getTierIntervals.R
@@ -2,7 +2,7 @@
 #'
 #' This function returns a data frame of the Intervals from an Interval Tier.
 #' @param interval_tier an IntervalTier returned from getTierByName() or getTierByNumber()
-#' @param discard_empty boolean TRUE (default) or false. Indicates whether intervals with no text in them should be discarded.
+#' @param discard_empty boolean FALSE (default) or TRUE. Indicates whether intervals with no text in them should be discarded in selection.
 #' @export
 #' @examples
 #' # Get intervals from "word" tier from data in mytg.TextGrid
@@ -15,7 +15,7 @@
 #'    getTierByName("word") %>%
 #'    getTierIntervals()
 
-getTierIntervals <- function(interval_tier, discard_empty = TRUE) {
+getTierIntervals <- function(interval_tier, discard_empty = FALSE) {
   if(interval_tier$class != "IntervalTier") {
     stop("Tier is not an interval tier!")
   }
